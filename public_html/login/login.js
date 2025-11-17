eed9cbdb27a57dfc29e71a03087722c95e598015
Tusharpaul231 <paultushar587@gmail.com>
2025-11-18 03:12:47 +0530

    /api fixed

--- public_html/login/login.js
+++ public_html/login/login.js
@@ -1,7 +1,7 @@
 const API_BASE =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "http://localhost:4000/api/login"
-    : "https://the-alpha-network-backend.onrender.com/api/login";
+    ? "http://localhost:4000/api"
+    : "https://the-alpha-network-backend.onrender.com/api";
 
 
 // -------------------------------
